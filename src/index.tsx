a13e3a8d827fc3b490bc3bd2f11980941900c03e
afif <afif@garasilabs.com>
2022-04-22 16:31:11 +0700

    feat: :sparkles: add new basic component & reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,15 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import {
-  createTheme,
-  MuiThemeProvider,
-  CssBaseline,
-} from '@material-ui/core';
-
-import App from './App';
-
-import './index.css';
-
-// override theme
-const theme = createTheme({
-  palette: {
-    type: 'dark',
-  },
-});
-
-render(
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </MuiThemeProvider>,
-  document.getElementById('app-root')
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
