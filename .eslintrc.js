b8a873915862f1c2787fb8e4649d3dccdf5df76f
afif <afif@garasilabs.com>
2022-04-22 16:27:51 +0700

    chore: :hammer: update ES Lint development scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-undef */
 module.exports = {
+  "overrides": [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      }
+    }
+  ],
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import'],
   extends: [
     'airbnb-typescript',
     'prettier',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended'
   ],
 };
