f9723f075b0d98972f1b01e5a53e37101c200564
afif <afif@garasilabs.com>
2022-05-06 17:49:44 +0700

    fix: :bug: can't import png file
    add custom module declaration for specific extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import logo from "./logo.png";
-import "./App.css";
-import { Box, Typography } from "@mui/material";
+import React from 'react';
+import logo from 'src/logo.png';
+import './App.css';
+import { Box, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Box className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant="body1">
+    <Box className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Typography variant='body1'>
           Edit <code>src/App.tsx</code> and save to reload.
         </Typography>
       </header>
